fc19b875be28aa87201b9c6c9c662ac4297611d2
jayanga <jay.chandrakumara@hotmail.com>
2023-07-22 14:00:36 +0800

    adding blog post module

--- Models/DTO/CategoryDto.cs
+++ Models/DTO/CategoryDto.cs
@@ -1,9 +1,13 @@
-﻿namespace CodePulse.API.Models.DTO
+﻿using CodePulse.API.Models.Domain;
+
+namespace CodePulse.API.Models.DTO
 {
     public class CategoryDto
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string UrlHandle { get; set; }
+        public ICollection<BlogPostDto> BlogPosts { get; set; } = new List<BlogPostDto>();
+
     }
 }
